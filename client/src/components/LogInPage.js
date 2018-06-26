0a9ce8f95f46b47db4627fce6b30d84ed2622f68
starryknight <mrstanleykimani@gmail.com>
2018-06-26 15:10:11 -0400

    made changes to logIn

--- client/src/components/LogInPage.js
+++ client/src/components/LogInPage.js
@@ -1,36 +1,75 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import axios from 'axios'
 
-class LogInPage extends Component {
-    state = {
-      userName: "",
-      password: ""  
-    }
-    render() {
-        return (
-            <div>
-                <form action="">
-                <input type="text"
-                placeholder = "Name"
-                name="userName"
-                value={this.state.userName}
-            onChange={this.handleChange}
+// We'll need to build a link to the idea board for each user in the db
 
-                />
-    
-                <br/>
-                <input type="password"
-                placeholder = "password"
-                name="userName"
-                />
-                <br/>
-                <button type="submit">Submit</button>
-                
-                </form>
-            </div>
-        );
-    }
+
+// We'll also need the ability to create a user
+
+class LogInPage extends Component {
+  state = {
+    userName: '',
+    password: ''
+  }
+
+  handleChange = (event) => {
+    const inputName = event.target.name
+    const userInput = event.target.value
+
+    // alt way of setting state
+    // this.setState({
+    //   [ inputName ]: userInput
+    // })
+
+    const newState = { ...this.state }
+    newState[ inputName ] = userInput
+    this.setState(newState)
+
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    axios.post('/api/users', this.state).then((res) => {
+      console.log(res.data)
+      this.props.history.push(`/users/${res.data._id}`)
+    })
+
+    // make api call
+  }
+
+  render () {
+    return (
+      <div>
+        <h1>Please Select A User</h1>
+        {this.props.users.map((user) => {
+          return (
+            <Link key={user._id} to={`/user/${user._id}`}>{user.userName}</Link>
+          )
+        })}
+
+        <h3>Create a User</h3>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            placeholder="User Name"
+            type="text"
+            name="userName"
+            value={this.state.userName}
+            onChange={this.handleChange}
+          />
+          <input
+            placeholder="Password"
+            type="password"
+            name="password"
+            value={this.state.password}
+            onChange={this.handleChange}
+          />
+
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    )
+  }
 }
 
-export default LogInPage;
+export default LogInPage
