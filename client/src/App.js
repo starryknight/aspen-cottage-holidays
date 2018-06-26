2d210d4664fd8aa151a8ded82d3ca31f57f3089a
starryknight <mrstanleykimani@gmail.com>
2018-06-25 20:05:48 -0400

    added components

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      
+      <img src="https://www.brandcrowd.com/gallery/brands/pictures/picture14299092496285.png" alt=""/>
        <h1>Aspen Cottage Holidays</h1>
       </div>
     );
