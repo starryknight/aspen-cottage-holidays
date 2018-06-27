e12ed662e5e8570b75887116db94782f2bae3460
starryknight <mrstanleykimani@gmail.com>
2018-06-27 14:25:50 -0400

    changed userpage by adding axios call

--- client/src/components/UserPage.js
+++ client/src/components/UserPage.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 
 class UserPage extends Component {
+    state ={
+        user:{}
+    }
+    componentDidMount () {
+        if (this.props.match.params) {
+            const userId = this.props.match.params.userId
+            console.log(userId)
+            axios
+                .get(`/api/users/${userId}`)
+                .then(res => {
+                    this.setState({user: res.data})
+                })
+        }
+    }
+      
     render() {
+        const user = this.state.user
+        console.log('user', user.user.userName)
         return (
             <div>
                 <p>hello user</p>
-                <p>{this.props.user}</p>
+                {user.userName}
             </div>
         );
     }
