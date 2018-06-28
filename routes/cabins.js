963d04ddbfcb19ad016a8b0ad8cb441f1a677ea3
starryknight <mrstanleykimani@gmail.com>
2018-06-28 17:03:45 -0400

    finished delete route

--- routes/cabins.js
+++ routes/cabins.js
@@ -1,55 +1,62 @@
-let express = require('express')
-let router = express.Router({ mergeParams: true })
-const { UserModel, CabinModel } = require('../db/schema')
+let express = require("express");
+let router = express.Router({ mergeParams: true });
+const { UserModel, CabinModel } = require("../db/schema");
 //show
-router.get('/', function (req, res) {
-    CabinModel.find().then((cabins) => {
-      res.send(cabins)
-    })
-  })
+router.get("/", function(req, res) {
+  CabinModel.find().then(cabins => {
+    res.send(cabins);
+  });
+});
 //new
-router.post('/new', function (req, res) {
+router.post("/new", function(req, res) {
+  const newCabin = new CabinModel(req.body);
 
-    const newCabin = new CabinModel(req.body)
-
-  UserModel.findById(req.params.userId).then((user) => {
-    user.cabins.push(newCabin)
-    return user.save()
-  }).then(savedCabin => {
-    res.send({
-      newCabin
+  UserModel.findById(req.params.userId)
+    .then(user => {
+      user.cabins.push(newCabin);
+      return user.save();
     })
-  })
-})
-
-
-router.patch('/:id', async (req, res) => {
-  const user = await UserModel.findById(req.params.userId)
-
- 
-  const ideaId = req.params.id
-
-
-  const cabinToEdit = user.cabins.id(ideaId)
-  cabinToEdit.title = req.body.title
-  cabinToEdit.description = req.body.description
-
-  // save that to db
-  const savedUser = await user.save()
-  res.send({
-    user: savedUser
-  })
-})
-
-
-router.delete('/:id', async (req, res) => {
-  const user = await UserModel.findById(req.params.userId)
+    .then(savedCabin => {
+      res.send(newCabin);
+    });
+});
+// limit: Number,
+//   address: String,
+//   city: String,
+//   state: String,
+//   Availability: Date,
+//   picture: String,
+//   smoking: Boolean,
+//   activities: [ActivitySchema]
+//update
+router.patch("/:id", function(req, res) {
+    const updatedCabin = req.body
+    console.log(updatedCabin)
+  const cabinId = req.params.id
+  UserModel.findById(req.params.userId)
+    .then(user => {
+      const cabinToUpdate = user.cabins.id(cabinId);
+      cabinToUpdate.limit = req.body.limit
+      return user.save();
+    })
+    .then(() => {
+      res.send(updatedCabin);
+    });
+});
+//delete
+router.delete('/:id',  function (req, res) {
+   
+    UserModel.findById(req.params.userId)
+    
+    .then((user) => {
   user.cabins.id(req.params.id).remove()
-  const savedUser = await user.save()
-  res.send({
-    user: savedUser
-  })
+        return user.save()
+    })
+        .then((savedUser) => {
+            res.send({user: savedUser})
+                
+        })
+        .catch(err => console.log((err)))
 })
 
-
-module.exports = router
+module.exports = router;
